1180e060b31d1ee6b66efac9026d431ab227f51d
Furthir <furthirspam@gmail.com>
2024-06-08 00:24:33 -0700

    augh

--- docs/scroll.js
+++ docs/scroll.js
@@ -1,13 +1,28 @@
-var scrlUp = document.getElementById("scrlUp");
+var scrlbtn = document.getElementById("scrlUp");
+var rootElement = document.documentElement;
 
-var rootElement = document.documentElement
+window.addEventListener("scroll", dist);
+
+function dist() {
+  const btn = document.getElementById("scrlUp");
+  if (document.body.scrollTop > 80 || rootElement.scrollTop > 80 || window.scrollY > 80) {
+    btn.classList.add('visible');
+  } else {
+    btn.classList.remove('visible');
+  }
+}
+
+scrlbtn.addEventListener("click", scrlToTop);
 
 function scrlToTop() {
-rootElement.scrollTo({
-  top: 0,
+  rootElement.scrollTo({
+    top: 0,
   behavior: "smooth"
   })
 }
 
-scrlUp.addEventListener("click", scrlToTop)
+
+
+
+
 
